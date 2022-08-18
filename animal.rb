d283b0918cd07d33fb8cfaafbd7a2762dc80392e
AmosJune <amos.sifuma@student.moringaschool.com.com>
2022-08-18 13:04:04 +0300

    Finito

--- animal.rb
+++ animal.rb
@@ -1,3 +1,49 @@
 # TODO: CREATE CLASS AND INSTANCE METHODS (INCLUDE VARIABLES)
+class Animal
+    
+    # Reader Vs Accessor
+
+    @species
+    @life_expectancy
+
+    attr_reader :species
+    attr_writer :species
+
+    attr_accessor :life_expectancy
+
+    # Creating a getter for the animal
+    def name
+        # an instance var accessed with @
+        @name        
+    end
+
+    # Setter for the animal
+    def name=(name)
+        @name = name
+    end
+
+    def sound
+        puts "#{self.name} is sounding"        
+    end
+
+end
+
+dog = Animal.new
+dog.name = "Simba"
+dog.sound
+dog.species = "Chihuahua"
+dog.life_expectancy = 13
+puts dog.species
+
+cat = Animal.new
+cat.name = "Meow"
+puts cat
+cat.sound
+
+lioness = Animal.new
+lioness.name = "Nala"
+# retrieve the name of the animal
+puts lioness.name
+lioness.sound
 
 # TODO: Use self to access instance variables
--- plant.rb
+++ plant.rb
@@ -1,1 +1,38 @@
 # TODO: Implement a class do illustrate how class variables and methods work.
+class Plant 
+    @@num_of_trees = 0
+
+    attr_accessor :species
+
+    def initialize(species)
+        @species = species
+        @@num_of_trees += 1
+    end
+
+    def speak_of_species
+        puts "#{self.species} is speaking"
+    end
+
+    # A Getter [class method]
+    def self.tree_count
+        @@num_of_trees
+    end
+
+end
+
+# ACCESS CLASS VAR
+mwarubaini = Plant.new("Neem Tree")
+mwarubaini.speak_of_species
+puts Plant.tree_count
+
+# puts mwarubaini.tree_count
+
+eucalyptus = Plant.new("eucalyptus Tree")
+eucalyptus = Plant.new("eucalyptus Tree")
+cypress = Plant.new("cypress Tree")
+oak = Plant.new("oak Tree")
+orange = Plant.new("orange Tree")
+
+puts Plant.tree_count
+
+# puts eucalyptus.tree_count
